343ade88fed2b5654080dd1f67432d20d1fc18fa
Ryan Drewery <ryan.drewery@me.com>
2016-10-03 08:47:56 +0100

    Initial Commit

---
+++ Mr RyanTests/Mr_RyanTests.swift
@@ -0,0 +1,36 @@
+//
+//  Mr_RyanTests.swift
+//  Mr RyanTests
+//
+//  Created by Ry & Nat on 03/10/2016.
+//  Copyright © 2016 Ry & Nat. All rights reserved.
+//
+
+import XCTest
+@testable import Mr_Ryan
+
+class Mr_RyanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
